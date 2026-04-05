290a8d1788e9c6cd2747fd8526c9c27bf4fda52a
prajit <prajitkumaran100@gmail.com>
2026-04-05 22:14:19 +0530

    Fix build errors for Vercel deployment

--- esevai/prisma/prisma.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'prisma';
-
-export default defineConfig({
-  schema: './schema.prisma',
-  db: {
-    url: process.env.DATABASE_URL,
-    directUrl: process.env.DIRECT_URL,
-  },
-});
